b261509326382b19cc3ef25c546b196ea6592204
cio <cio@mitlab.kr>
2019-05-20 22:27:38 +0900

    Delete User Info

--- api/routers/user.js
+++ api/routers/user.js
@@ -48,4 +48,23 @@ router.post("/signup", (req, res) => {
     .catch();
 });
 
+//사용자 계정 삭제
+router.delete("/:userId", (req, res) => {
+    userModel
+    .remove( {_id: req.params.userId})
+    .exec()
+    .then( result => {
+        res.status(200).json({
+            usr_msg: "사용자 삭제됨"
+        });
+    })
+    .catch( err => {
+        console.log(err);
+        res.status(500).json({
+            usr_err: "삭제중 오류 발생"
+        });
+    });
+});
+
+
 module.exports = router;
